74929012ba1ba7e523c3be389bc67ce1ed88f94d
Markobanduka <banduka44@gmail.com>
2024-06-11 10:39:24 +0200

    UI-design

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import HomePage from "./components/pages/HomePage";
+import { Route, Routes } from "react-router-dom";
+import FavoritesPage from "./components/pages/FavoritesPage";
+
+const App = () => {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
