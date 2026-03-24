01bd45655a74261b97763a519e2e5be9284ed392
Markobanduka <banduka44@gmail.com>
2026-03-24 17:42:24 +0100

    add logo test

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,7 +1,17 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("Recipe HomePage", () => {
+  it("renders the default elements on HomePage", () => {
     cy.visit("https://recipe-app-eight-bay.vercel.app/");
 
-    cy.get('[data-testid="cypress-title"]', { timeout: 10000 }).should("exist");
+    cy.get('[data-testid="cypress-title"]')
+      .should("exist")
+      .should("have.text", "Recommended Recipes");
+  });
+
+  it("renders the default elements on Sidebar", () => {
+    cy.visit("https://recipe-app-eight-bay.vercel.app/");
+
+    cy.get('[data-testid="cypress-logo-image"]')
+      .should("exist")
+      .should("have.attr", "src", "/logo.svg");
   });
 });
